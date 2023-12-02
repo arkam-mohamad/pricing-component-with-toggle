6473bfc2d79357a62d4bc4f14e83811ff76b54f9
arkam-mohamad <125960377+arkam-mohamad@users.noreply.github.com>
2023-12-02 19:40:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+let toggle = $('.toggle');
+let month = $('.month');
+let annual = $('.annual');
 
+$(toggle).click(function (e) { 
+  e.preventDefault();
+  $(toggle).toggleClass('active');
+  $(month).toggleClass('active');
+  $(annual).toggleClass('active');
+});
